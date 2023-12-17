a0282ef5d4d9a414a3da70e7160e647ff7b1482b
Pauli Selenius <99865089+Mera212@users.noreply.github.com>
2023-12-17 18:39:43 +0200

    vite config

--- src/assets/vite.config.js
+++ src/assets/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/muistipeli2",
+  base: "./muistipeli2",
   plugins: [react()],
 });
